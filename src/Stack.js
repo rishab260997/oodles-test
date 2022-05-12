96ddb919389b029e446da9e831f68d3875dee152
rishab260997 <rishab260997@gmail.com>
2022-05-12 15:32:47 +0530

    Signup Page created Stack navigation added Libraries installed

---
+++ src/Stack.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import Signup from './screens/Signup';
+import Thankyou from './screens/Thankyou';
+
+const Stack = createStackNavigator();
+
+const StackNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="ThankYou" component={Thankyou} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default StackNavigation;
---
+++ src/screens/Thankyou.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+
+const Thankyou = () => {
+    return(
+        <View>
+            <Text>
+                Thanks
+            </Text>
+        </View>
+    )
+}
+
+export default Thankyou
